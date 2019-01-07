bad2f337c96ac6ff952f7d89bdc4d2dce24aeb58
Jesse Boyce <jesse@jpboyce.org>
2019-01-08 05:30:18 +1000

    Created file

---
+++ chef/cb_baseline/recipes/20-iis.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_baseline
+# Recipe:: 20-iis
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
---
+++ chef/cb_baseline/recipes/30-sqlserver.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cb_baseline
+# Recipe:: 30-sqlserver
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
